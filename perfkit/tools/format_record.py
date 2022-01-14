b13028dd388d7f3fbb58f5313352d14ed5cffad9
Aarni Koskela <akx@iki.fi>
2022-01-14 12:30:10 +0200

    Add GFM formatting

--- perfkit/tools/format_record.py
+++ perfkit/tools/format_record.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 import sys
-from typing import List
+from typing import List, Any, Dict, Iterable
 
 
 def flatten(datum: dict, *, remove_time: bool = False):
@@ -37,16 +37,36 @@ def compute_pcts(data, fields: List[str]):
         }
 
 
+def get_keys_in_order(dicts: Iterable[Dict[str, Any]]) -> List[str]:
+    seen = set()
+    keys = []
+    for d in dicts:
+        new_keys = [k for k in d if k not in seen]
+        if new_keys:
+            keys.extend(new_keys)
+            seen.update(new_keys)
+    return keys
+
+
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("--remove-time", action="store_true", default=False)
     ap.add_argument("--pct-field", dest="pct_fields", action="append", default=[])
+    ap.add_argument("--format", choices=("json", "gfm"), default="json")
     args = ap.parse_args()
     data = [
         flatten(json.loads(line), remove_time=args.remove_time) for line in sys.stdin
     ]
     data = list(compute_pcts(data, fields=args.pct_fields))
-    print(json.dumps(data, sort_keys=True, ensure_ascii=False))
+    if args.format == "gfm":
+        keys = get_keys_in_order(data)
+        print(f"| {' | '.join(map(str, keys))} |")
+        print(f"| {' | '.join(['---'] * len(keys))} |")
+        for d in data:
+            print(f"| {' | '.join(str(d.get(key, '')) for key in keys)} |")
+
+    elif args.format == "json":
+        print(json.dumps(data, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == "__main__":
